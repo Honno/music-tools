9d0204cdecbdea7d9d4f4880acde31e4a2628193
Matthew Barber <quitesimplymatt@gmail.com>
2018-09-29 21:14:44 +0100

    playlists now named after folder

--- organise/playlist-gen.py
+++ organise/playlist-gen.py
@@ -3,16 +3,28 @@
 import os
 
 FILE_TYPES = ['mp3', 'flac', 'ogg']
-PLAYLIST_FILE = 'bestof.m3u'
+EXTENSION = '.m3u'
+TOP_PLAYLIST_NAME = 'All'
 
 def is_audio(name):
     return any(name.endswith(file_type) for file_type in FILE_TYPES)
 
+def current_folder(path):
+    dirs = path.split('/')
+    if len(dirs) > 0:
+        return dirs[-1]
+    else:
+        return TOP_PLAYLIST_NAME
+
 if __name__ == "__main__":
+
     for root, folders, files in os.walk('.', topdown=False):
-        print("\nIn {}".format(root))
-        if not root.startswith('./.') and PLAYLIST_FILE not in files:
-            with open(root+'/'+PLAYLIST_FILE, 'w+') as playlist:
+        folder = current_folder(root)
+        playlist_name = folder + EXTENSION
+        print("In {}".format(root))
+
+        if not root.startswith('./.') and playlist_name not in files:
+            with open(root+'/'+playlist_name, 'w+') as playlist:
                 print("No playlist file found, creating one")
                 audio_files = filter(is_audio, files)
                 for filename in audio_files:
@@ -20,10 +32,10 @@ if __name__ == "__main__":
 
                 for folder in folders:
                     try:
-                        sub_playlist = open(root+'/'+folder+'/'+PLAYLIST_FILE, 'r')
+                        sub_playlist = open(root+'/'+folder+'/'+folder+EXTENSION, 'r')
                         print("Sub playlist file found in {}, adding to current playlist".format(folder))
                         for song in sub_playlist:
                             playlist.write(folder+'/'+song)
                         sub_playlist.close()
-                    except IOError:
-                        pass
+                    except IOError as e:
+                        print(e)
